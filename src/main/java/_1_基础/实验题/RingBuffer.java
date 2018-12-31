181a39120a025ff2ae1e2f086f464bc808e37c2b
oleileiA <18604469853@163.com>
2018-12-31 13:11:15 +0800

    RingBuffer

--- src/main/java/_1_基础/实验题/RingBuffer.java
+++ src/main/java/_1_基础/实验题/RingBuffer.java
@@ -8,11 +8,19 @@ package _1_基础.实验题;
 *
 * 1. new RingBuffer(int cap) // 新建一个环形缓存
 * 2. clear 清空 ringBuffer 函数
-* 3. 读数据函数
-* 4. 写数据函数
+* 3. 读数据函数 get
+* 4. 写数据函数 put
 * 5. 获取 ringBuffer 的长度
+* 6.
+* 特点：
+* 大小是固定的所以没有resize
 *
-* 和ResizingArrayDeque感觉差不多
+*
+* 有在循环队列中永远预留一个空位的方式是为了区别判断条件
+* 去判断队列是空的： head == tail
+* 还是满的       ： head == (tail + 1) % a.length
+*
+* 但是我们的实现中记录个元素的个数N，所以不需要这样判断
 * */
 public class RingBuffer<Item> {
 
@@ -24,19 +32,8 @@ public class RingBuffer<Item> {
 
     private int tail;
 
-    private void resize(int len) {
-        Item[] temp = (Item[]) new Object[len];
-        for (int i = 0; i < N; i++) {
-            temp[i] = a[(head + i) % a.length];
-        }
-        a = temp;
-        head = 0;
-        tail = N;
-    }
-
     public RingBuffer(int cap) {
         a = (Item[]) new Object[cap];
-        N = cap;
     }
 
     public boolean isEmpty() {
@@ -47,25 +44,55 @@ public class RingBuffer<Item> {
         return N;
     }
 
-    // 右入列
-    public void pushRight(Item item) {
-        if (N == a.length) resize(a.length * 2);
-        // 这里维护的tail的大小总是在数组之内，我感觉没有必要，可以大于数组长度。
-        // 在赋值的时候计算就可以了，但本质上没有区别
+    public void put(Item item) {
+        assert size() != N : "满了";
         a[tail] = item;
         tail = (tail + 1) % a.length;
         N++;
     }
 
-    // 左出列
-    public Item popLeft() {
+    public Item get() {
+        assert N == 0 : "空了";
         Item item = a[head];
-        a[head] = null;
         head = (head + 1) % a.length;
         N--;
-        if (N == a.length / 4) resize(a.length / 2); // 可以放在后面
         return item;
     }
 
+    public void clear() {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = null;
+        }
+        N = 0;
+        head = 0;
+        tail = 0;
+    }
 
+    public Item[] getAll() {
+        Item[] temp = (Item[]) new Object[N];
+        int i = 0;
+        while (!isEmpty()) {
+            temp[i] = get();
+            i++;
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        RingBuffer<Integer> ringBuffer = new RingBuffer<Integer>(10);
+        ringBuffer.put(1);
+        ringBuffer.put(2);
+        ringBuffer.put(3);
+        ringBuffer.put(4);
+        ringBuffer.put(5);
+        ringBuffer.put(6);
+        ringBuffer.put(7);
+        ringBuffer.put(8);
+        ringBuffer.put(9);
+        ringBuffer.put(10);
+
+        while (!ringBuffer.isEmpty()) {
+            System.out.println(ringBuffer.get());
+        }
+    }
 }
